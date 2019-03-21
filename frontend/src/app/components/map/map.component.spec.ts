b41e51447511c2b1044579a7040bcc36e3800e9e
Oleg Konev <wgarland@yandex.ru>
2019-03-21 03:34:17 +0200

    :rotating_light: create unit-test for rotateMarker() method

--- frontend/src/app/components/map/map.component.spec.ts
+++ frontend/src/app/components/map/map.component.spec.ts
@@ -1,6 +1,9 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MapComponent } from './map.component';
+import {MapConstants} from "./map-constants";
+import {AgmCoreModule} from "@agm/core";
+import {AgmSnazzyInfoWindowModule} from "@agm/snazzy-info-window";
 
 describe('MapComponent', () => {
   let component: MapComponent;
@@ -8,7 +11,13 @@ describe('MapComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ MapComponent ]
+      declarations: [ MapComponent ],
+      imports: [
+        AgmCoreModule.forRoot({
+          apiKey: 'API_KEY'
+        }),
+        AgmSnazzyInfoWindowModule
+      ]
     })
     .compileComponents();
   }));
@@ -21,5 +30,51 @@ describe('MapComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should be equal', () => {
+    component.markers = [
+      {
+        lat: 12.09407,
+        lng: 26.31618,
+        alpha: 1,
+        icon: {
+          path: MapConstants.MARKERS_ICONS['hospital'],
+          rotation: 0,
+          fillColor: 'black',
+          strokeColor: 'black',
+          fillOpacity: 1,
+          strokeOpacity: 1,
+          scale: 1,
+          strokeWeight: 1,
+          anchor: {
+            x: 22,
+            y: 22
+          }
+        },
+      },
+      {
+        lat: 47.92393,
+        lng: 78.58339,
+        alpha: 1,
+        icon: {
+          path: MapConstants.MARKERS_ICONS['gasStation'],
+          rotation: 0,
+          fillColor: 'black',
+          strokeColor: 'black',
+          fillOpacity: 1,
+          strokeOpacity: 1,
+          scale: 1,
+          strokeWeight: 1,
+          anchor: {
+            x: 22,
+            y: 22
+          }
+        },
+      }
+    ];
+    component.selectedMarker = 1;
+    component.rotateMarker(300);
+    expect(component.markers[0].icon.rotation).toEqual(300);
   });
 });
