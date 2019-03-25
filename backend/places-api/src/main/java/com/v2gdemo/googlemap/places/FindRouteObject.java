c88c754bd466c8c82d0e22f860141ee512b3f2e9
NothingIsMatter <sashalays1337@gmail.com>
2019-03-25 15:10:02 +0200

    pom.xml added

---
+++ backend/places-api/src/main/java/com/v2gdemo/googlemap/places/FindRouteObject.java
@@ -0,0 +1,5 @@
+package com.v2gdemo.backend.places;
+
+public interface FindRouteObject {
+    String findRouteBetween(String from,String to);
+}
---
+++ backend/places-api/src/main/java/com/v2gdemo/googlemap/security/UserDetailsService.java
@@ -0,0 +1,27 @@
+package com.v2gdemo.backend.security;
+
+import com.v2gdemo.backend.dao.UserDao;
+import com.v2gdemo.backend.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+
+public class UserDetailsService implements org.springframework.security.core.userdetails.UserDetailsService {
+    @Autowired
+    private UserDao userDao;
+    @Autowired
+    private PasswordEncoder encoder;
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+          User user = userDao.findByLogin(s);
+        org.springframework.security.core.userdetails.User.UserBuilder userBuilder = null;
+          if (user != null){
+              userBuilder = org.springframework.security.core.userdetails.User.withUsername(s);
+              userBuilder.password(user.getPassword());
+              userBuilder.roles(user.getRoles().toString());
+          }
+        return userBuilder.build();
+    }
+}
