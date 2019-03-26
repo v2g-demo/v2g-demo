7be4730fa90f7af71e5018200e8ca6a048b60b7a
inferal <pyurik44@gmail.com>
2019-03-26 20:48:03 +0300

    Выполнено На главной навигационной панели должны быть ссылки:
    Getting Started - переименовать в V2G Demo и роутить на главную страницу
    Components - переименовать в About и роутить на страницу About

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,14 +4,16 @@ import { WelcomeComponent } from './components/welcome/welcome.component';
 import {NotFoundComponent} from './components/not-found/not-found.component';
 import {MapComponent} from './components/map/map.component';
 import {GameSelectorComponent} from './components/game-selector/game-selector.component';
+import { AboutComponent } from './components/about/about.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'home' },
   { path: 'home', component: GameSelectorComponent },
+  { path: 'about', component: AboutComponent},
   { path: 'map', component: MapComponent },
   { path: 'map/:id', component: MapComponent },
-  {path: '404', component: NotFoundComponent},
-  {path: '**', redirectTo: '404'},
+  { path: '404', component: NotFoundComponent},
+  { path: '**', redirectTo: '404'},
 ];
 
 @NgModule({
