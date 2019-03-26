61fac64954922b13a9ed069bfc49968063e6105b
wgarland <WAcHimyiadHijr0>
2019-03-26 04:56:43 +0200

    :hammer: remove types for dev.api constants

--- src/app/api/dev.api.ts
+++ src/app/api/dev.api.ts
@@ -1,2 +1,2 @@
-export const API_HOST: string = 'http://localhost:4200';
-export const MOCK_DATA: string = `${API_HOST}/assets/mock-data`;
+export const API_HOST = 'http://localhost:4200';
+export const MOCK_DATA = `${API_HOST}/assets/mock-data`;
