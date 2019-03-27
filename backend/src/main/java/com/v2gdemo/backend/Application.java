c9964d39c6417ffe2f3e8b32fb5ad7943937ec16
Andrey Bronin <andrey.bronin@insolar.io>
2019-03-27 16:05:50 +0300

    rename Application

--- backend/src/main/java/com/v2gdemo/backend/Application.java
+++ backend/src/main/java/com/v2gdemo/backend/Application.java
@@ -10,10 +10,10 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @Configuration
 @EnableWebMvc
 @EnableJpaRepositories
-public class GooglemapApplication {
+public class Application {
 
 
     public static void main(String[] args) throws Exception {
-        SpringApplication.run(GooglemapApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 }
