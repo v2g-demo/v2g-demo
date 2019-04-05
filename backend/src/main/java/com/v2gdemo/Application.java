c83fe63a10222b8f02001f72ac0aa244eb3ae837
Andrey Bronin <andrey.bronin@insolar.io>
2019-04-05 22:56:49 +0300

    move to postresql

--- backend/src/main/java/com/v2gdemo/Application.java
+++ backend/src/main/java/com/v2gdemo/Application.java
@@ -2,12 +2,15 @@ package com.v2gdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+@Configuration
+@ComponentScan
 @SpringBootApplication
 @EnableWebMvc
 @EnableScheduling
