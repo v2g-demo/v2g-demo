43ded14a8c149124400587e8982178f196a26fa3
Andrey Bronin <andrey.bronin@insolar.io>
2019-05-30 23:23:59 +0300

    Fix e2e

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -12,7 +12,7 @@ describe('new App', () => {
     });
     it('should have a title saying Home', () => {
       page.getPageOneTitleText().then(title => {
-        expect(title).toEqual('Home');
+        expect(title).toEqual('Select the game');
       });
     });
   });
