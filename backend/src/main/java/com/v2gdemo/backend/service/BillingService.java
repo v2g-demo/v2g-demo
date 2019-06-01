092bde0021b03e0d57a2fc192f3e952495223c17
NothingIsMatter <sashalays1337@gmail.com>
2019-06-01 11:51:50 +0300

    New tests added

--- backend/src/main/java/com/v2gdemo/backend/service/BillingService.java
+++ backend/src/main/java/com/v2gdemo/backend/service/BillingService.java
@@ -25,7 +25,7 @@ public class BillingService {
 
     long toBalance = to.getBalance();
 
-
+ 
     fromBalance -= value;
     toBalance += value;
 
--- backend/src/test/java/com/v2gdemo/backend/service/BillingServiceTest.java
+++ backend/src/test/java/com/v2gdemo/backend/service/BillingServiceTest.java
@@ -9,95 +9,91 @@ import static org.junit.Assert.*;
 
 public class BillingServiceTest {
 
-  private ObjectRepository objectRepository = mock(ObjectRepository.class);
+  private WalletRepository walletRepository = mock(WalletRepository.class);
   private TransactionRepository transactionRepository = mock(TransactionRepository.class);
 
   @Test
   public void makeTransaction() {
-//
-//    Wallet wallet = new Wallet();
-//    wallet.setBalance(40000);
-//
-//    Wallet walletForCar = new Wallet();
-//    walletForCar.setBalance(200000);
-//
-//
-//    Object car = new Object();
-//    car.setWallet(walletForCar);
-//
-//
-//    long value = 30000;
-//    String address = "some address";
-//
-//    when(transactionRepository.save(wallet)).thenReturn(null);
-//    when(objectRepository.save(car)).thenReturn(null);
-//    when(walletRepository.findByAddress(address)).thenReturn(wallet);
-//
-//    BillingService billingService = new BillingService(transactionRepository,objectRepository);
-//    try {
-//      billingService.makeTransaction(car, address, value);
-//
-//    } catch (Exception ex){ex.printStackTrace();}
+
+    String firstAddress= "firstAddress";
+    String secondAddresss= "secondAddress";
+
+    Wallet firstWallet = new Wallet();
+    firstWallet.setBalance(200000);
+
+    Wallet secondWallet = new Wallet();
+    secondWallet.setBalance(0);
+
+
+
+    Long value = 30000L;
+    String address = "some address";
+
+    when(walletRepository.findByAddress(firstAddress)).thenReturn(firstWallet);
+    when(walletRepository.findByAddress(secondAddresss)).thenReturn(secondWallet);
+
+    BillingService billingService = new BillingService(walletRepository,transactionRepository);
+    try {
+      Transaction transaction = billingService.makeTransaction(firstAddress, secondAddresss, value);
+      assertEquals(transaction.getValue(),value);
+      assertEquals(transaction.getSource(),firstWallet);
+    } catch (Exception ex){ex.printStackTrace();}
+
   }
 
   @Test
   public void makeTransactionWithoutMoney() {
 
-//    Wallet wallet = new Wallet();
-//    wallet.setBalance(40000);
-//
-//    Wallet walletForCar = new Wallet();
-//    walletForCar.setBalance(1000);
-//
-//
-//    Object car = new Object();
-//    car.setWallet(walletForCar);
-//
-//
-//    long value = 30000;
-//    String address = "some address";
-//
-//    when(walletRepository.save(wallet)).thenReturn(null);
-//    when(objectRepository.save(car)).thenReturn(null);
-//    when(walletRepository.findByAddress(address)).thenReturn(wallet);
-//
-//    BillingService billingService = new BillingService(walletRepository, objectRepository);
-//    try {
-//      billingService.makeTransaction(car, address, value);
-//
-//    } catch (Exception ex) {
-//      ex.printStackTrace();
-//    }
-//  }
-//
-//  @Test
-//  public void makeTransactionWithWrongAddress() {
-//
-//    Wallet wallet = new Wallet();
-//    wallet.setBalance(40000);
-//
-//    Wallet walletForCar = new Wallet();
-//    walletForCar.setBalance(1000);
-//
-//
-//    Object car = new Object();
-//    car.setWallet(walletForCar);
-//
-//
-//    long value = 30000;
-//    String address = "some address";
-//
-//    when(walletRepository.save(wallet)).thenReturn(null);
-//    when(objectRepository.save(car)).thenReturn(null);
-//    when(walletRepository.findByAddress(address)).thenReturn(null);
-//
-//    BillingService billingService = new BillingService(walletRepository, objectRepository);
-//    try {
-//      billingService.makeTransaction(car, address, value);
-//
-//    } catch (Exception ex) {
-//      ex.printStackTrace();
-//    }
+    String firstAddress= "firstAddress";
+    String secondAddresss= "secondAddress";
+
+    Wallet firstWallet = new Wallet();
+    firstWallet.setBalance(10000);
+
+    Wallet secondWallet = new Wallet();
+    secondWallet.setBalance(0);
+
+
+
+    Long value = 30000L;
+    String address = "some address";
+
+    when(walletRepository.findByAddress(firstAddress)).thenReturn(firstWallet);
+    when(walletRepository.findByAddress(secondAddresss)).thenReturn(secondWallet);
+
+    BillingService billingService = new BillingService(walletRepository,transactionRepository);
+    try {
+      Transaction transaction = billingService.makeTransaction(firstAddress, secondAddresss, value);
+      fail(); // Should have thrown an exception
+    } catch (Exception ex){ex.printStackTrace();}
+
+  }
+
+  @Test
+  public void makeTransactionWithWrongAddress() {
+
+    String firstAddress= "firstAddress";
+    String secondAddresss= "secondAddress";
+
+
+
+    Wallet secondWallet = new Wallet();
+    secondWallet.setBalance(0);
+
+
+
+    Long value = 30000L;
+    String address = "some address";
+
+    when(walletRepository.findByAddress(firstAddress)).thenReturn(null);
+    when(walletRepository.findByAddress(secondAddresss)).thenReturn(secondWallet);
+
+    BillingService billingService = new BillingService(walletRepository,transactionRepository);
+    try {
+      Transaction transaction = billingService.makeTransaction(firstAddress, secondAddresss, value);
+      fail(); // Should have thrown an exception
+    } catch (Exception ex){ex.printStackTrace();}
+
   }
 
 }
