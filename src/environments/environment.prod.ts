ea217eae6df8f48b465235b29bd2e950a416e935
Andrey Bronin <andrey.bronin@insolar.io>
2019-12-31 18:21:43 +0300

    build fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,13 @@ export const environment = {
   production: true,
   googleMapsApiKey: 'KEY',
   host: 'http://v2g-demo.com',
-  apiUrl: 'http://api.v2g-demo.com/api'
+  apiUrl: 'http://api.v2g-demo.com/api',
+  firebase: {
+    apiKey: 'AIzaSyCB-nfwTCeJ_113ak55NmqwqAv7I4BdQ1c',
+    authDomain: 'ionic-auth-example.firebaseapp.com',
+    databaseURL: 'https://ionic-auth-example.firebaseio.com',
+    projectId: 'ionic-auth-example',
+    storageBucket: '',
+    messagingSenderId: '994395349389'
+  }
 };
